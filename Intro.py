b3556966afb4d02f0f6ea1ce0ec097242c5b9df1
gwenlaura <gwenlovelyon1@gmail.com>
2025-07-30 11:33:35 +0300

    Data Structures $ Algorithms in Python

---
+++ Intro.py
@@ -0,0 +1,42 @@
+#PLP = 'Academy' 
+Student_Name = 'Abdi Kadir'
+Student_ID = 1001
+Student_Age = 20
+Fees_Clearance = True
+
+#output
+print(Student_Name, Student_Age, Student_ID, Fees_Clearance)
+
+#Lists
+languages = (1995, 'python', 'Java', 'C++', 'Javascript' )
+print (languages)
+
+shopping_list = ['Milk', 'Eggs', 'Bread', 'Butter']
+print (shopping_list)
+
+#Tuples
+products = ('XBox', 499.99, 'Habibi',23)
+print(products)
+
+#Sets
+Set_A = {1,2,3,4,5}
+Set_B = {4,5,6,7,8}
+print (Set_A)
+print (Set_B)
+
+#Dictionaries 
+# Student_info = {'Name': 'Abdi Kadir', 'ID':1001, 'Age':20.2, 'Fees Clearance':'True'}
+print (Student_info) 
+
+Capital_City = {'Kenya':'Nairobi'}
+print(Capital_City) 
+
+#Logical Operators
+a=10
+b=20
+c=30
+print(a<b and b<c) #True
+print(a<b) or (b<c) #True
+print(not (a<b)) #False
+
+
